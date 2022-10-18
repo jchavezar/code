45f864114e8df01c2eba8ab6d58414ac0c808452
Jesus <jchavezar@gmail.com>
2022-10-18 01:28:07 -0400

    latest

---
+++ pipe_notebook/bqml_tf_keras/app/main.py
@@ -0,0 +1,48 @@
+
+from fastapi import Request, FastAPI
+import json
+import os
+import sys
+import tensorflow as tf
+
+app = FastAPI()
+
+x=os.environ['AIP_STORAGE_URI']
+print(f'[INFO] ------ {x}', file=sys.stderr)
+
+# Loading Model File
+
+model = tf.keras.models.load_model(os.environ['AIP_STORAGE_URI'])
+
+# Webserver methods
+
+@app.get('/')
+def get_root():
+    return {'message': 'Welcome to Breast Cancer Prediction'}
+@app.get('/health_check')
+def health():
+    return 200
+if os.environ.get('AIP_PREDICT_ROUTE') is not None:
+    method = os.environ['AIP_PREDICT_ROUTE']
+else:
+    method = '/predict'
+print(method)
+@app.post(method)
+async def predict(request: Request):
+    print("----------------- PREDICTING -----------------")
+    body = await request.json()
+    instances = body["instances"]
+    columns = ["bounces", "channelGrouping", "country", "deviceCategory", "latest_ecommerce_progress", "medium", "pageviews", "source", "time_on_site"]
+    print(instances, file=sys.stderr)
+    zip_iterator = zip(columns, instances)
+    print(zip_iterator, file=sys.stderr)
+    sample = dict(zip_iterator)
+    print(sample, file=sys.stderr)
+    input_dict = {name: tf.convert_to_tensor([value]) for name, value in sample.items()}
+    predictions = model.predict(input_dict)
+    prob = tf.nn.sigmoid(predictions[0])
+    outputs = f"%.1f"%(100*prob)
+    print(f'[INFO] ------ {outputs}, {type(outputs)}', file=sys.stderr)
+    response = outputs
+    print("----------------- OUTPUTS -----------------")
+    return {"predictions": response}
